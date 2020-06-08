7b06852ed8d6d3f15e42f5d1be3f9559123f1eea
KrishnaMohan454 <ankeshwarkrishna@gmail.com>
2020-06-08 18:54:21 +0530

    Add files via upload
    added files manually

---
+++ TestFramework.framework/Headers/DataClass.h
@@ -0,0 +1,17 @@
+//
+//  DataClass.h
+//  TestFramework
+//
+//  Created by AID011 on 08/06/20.
+//  Copyright © 2020 AID011. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DataClass : NSObject
+
++(void)dataWantstoKnow;
+
+@end
+
+
---
+++ TestFramework.framework/Headers/TestFramework.h
@@ -0,0 +1,19 @@
+//
+//  TestFramework.h
+//  TestFramework
+//
+//  Created by AID011 on 08/06/20.
+//  Copyright © 2020 AID011. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <TestFramework/DataClass.h>
+//! Project version number for TestFramework.
+FOUNDATION_EXPORT double TestFrameworkVersionNumber;
+
+//! Project version string for TestFramework.
+FOUNDATION_EXPORT const unsigned char TestFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestFramework/PublicHeader.h>
+
+
